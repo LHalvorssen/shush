565564c78dc620461440fd3c52f1bdc5c6ca898b
Luis Halvorssen <luishalvorssen@gmail.com>
2019-08-15 11:38:06 -0400

    use postgre

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_01_193516) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.string "body"
     t.integer "user_id"
